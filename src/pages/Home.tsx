4b106fd3f4337dcbe834a89a9e7c39878a7de0b3
Ridham Taneja <ridham06042003@gmail.com>
2026-06-30 18:44:15 +0530

    feat(home): wire animated background, bento layout, and Skills section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,23 @@
+import Background from "../components/Background";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
+import Skills from "../components/Skills";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      <Projects />
-      <About />
-      <Contact />
+    <>
+      <Background />
+      <main className="relative z-10 pt-32 pb-16 px-5 md:px-16 max-w-[1440px] mx-auto w-full">
+        <Hero />
+        <Projects />
+        <Skills />
+        <About />
+        <Contact />
+      </main>
       <Footer />
-    </main>
+    </>
   );
 }
